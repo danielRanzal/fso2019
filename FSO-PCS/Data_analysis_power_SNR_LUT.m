4bed0fafc2f3d3cbdc22e2087bfd4818e787dee3
danielRanzal <48766077+danielRanzal@users.noreply.github.com>
2019-05-13 10:58:26 +0100

    Update Data_analysis_power_SNR_LUT.m

--- FSO-PCS/Data_analysis_power_SNR_LUT.m
+++ FSO-PCS/Data_analysis_power_SNR_LUT.m
@@ -52,31 +52,7 @@ zlabel('Attenuation[dB]');
 figure,hold on,plot(lut(:,1),lut(:,2)),plot(lut(:,1),SNR),hold off
 
 %% building lut
-function SNR =Lut(Power,lut)
 
-for reg=1:1:length(lut)
-    SNR=0;  % just assuring value is always assigned
-    if reg==1 && Power>lut(reg,1)   %extreme casses
-        SNR = funct2snr(reg,Power);
-    else if reg== length(lut)       %extreme casses
-            SNR = funct2snr(reg-1,Power);
-        else if Power >= lut(reg+1,1)   % usual expectable case
-                
-                SNR = funct2snr(reg,Power);
-                %      disp(Power)
-                % disp(lut(reg,1))
-                % pause(1)
-                break
-                
-            end
-        end
-    end
-    %if Power<= lut(reg,1)
-    
-    % break
-end
-
-end
 
 
 
